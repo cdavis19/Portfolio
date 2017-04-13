435e1755c7cedc9af7847b4ae2b853a9feac3d38
cdavis19 <cjdavis6791@gmail.com>
2017-04-13 15:25:56 -0400

    more changes

--- public/scripts/module.js
+++ public/scripts/module.js
@@ -4,15 +4,15 @@ app.config(function($routeProvider, $locationProvider){
   $routeProvider
     .when('/home', {
       controller: 'homeController',
-      templateUrl: 'views/home.html'
+      templateUrl: '..views/home.html'
     })
     .when('/about', {
       controller: 'aboutController',
-      templateUrl: 'views/about.html'
+      templateUrl: '..views/about.html'
     })
     .when('/portfolio', {
       controller: 'portfolioController',
-      templateUrl: 'views/portfolio.html'
+      templateUrl: '..views/portfolio.html'
     })
     .otherwise({redirectTo:'/home'});
     $locationProvider.hashPrefix('');
